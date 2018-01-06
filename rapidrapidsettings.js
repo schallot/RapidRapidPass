49224d4f07802695cdc3e812641ff1c5fd86c560
schallot <15661553+schallot@users.noreply.github.com>
2018-01-05 20:54:21 -0500

    Storing Q/A pairs in the config object.

--- rapidrapidsettings.js
+++ rapidrapidsettings.js
@@ -144,7 +144,123 @@ var getConfigFromDatastore = function(callback){
 	chrome.storage.sync.get(null, function(config){
 		if(!config){
 			console.log("No existing settings were found.");
+			config = {};
 		}
+		
+		if(!config.responses){
+			config.responses = [];
+		}
+		
+		var responses = 	[
+			{q:"Are you feeling healthy and well today?",
+				a:true},
+			{q:"Are you currently taking an antibiotic?",
+				a:false},
+			{q:"Are you currently taking any other medication for an infection?",
+				a:false},
+			{q:"Have you taken any medications on the Medication Deferral List in the time frames indicated? (Review the Medication Deferral List.)",
+				a:false},
+			{q:"Have you read the educational materials today?",
+				a:true},
+			{q:"In the past 72 hours have you taken aspirin or anything that has aspirin in it?",
+				a:false},
+			{q:"In the past 8 weeks have you donated blood, platelets or plasma?",
+				a:true},		
+			{q:"In the past 8 weeks, have you had any vaccinations or other shots?",
+				a:true},
+			{q:"In the past 8 weeks, have you had contact with someone who was vaccinated for smallpox in the past 8 weeks?",
+				a:false},
+			{q:"In the past 16 weeks have you donated a double unit of red cells using an apheresis machine?",
+				a:false},
+			{q:"In the past 12 months have you had a blood transfusion?",
+				a:false},
+			{q:"In the past 12 months have you had a transplant such as organ, tissue or bone marrow?",
+				a:false},
+			{q:"In the past 12 months have you had a graft such as bone or skin?",
+				a:false},
+			{q:"In the past 12 months have you come into contact with someone else's blood?",
+				a:false},
+			{q:"In the past 12 months have you had an accidental needle-stick?",
+				a:false},
+			{q:"In the past 12 months have you had sexual contact with anyone who has HIV/AIDS or has had a positive test for the HIV/AIDS virus?",
+				a:false},
+			{q:"In the past 12 months have you had sexual contact with a prostitute or anyone else who takes money or drugs or other payment for sex?",
+				a:false},
+			{q:"In the past 12 months have you had sexual contact with anyone who has ever used needles to take drugs or steroids, or anything NOT prescribed by,their doctor?",
+				a:false},
+			{q:"Male Donors: In the past 12 months, have you had sexual contact with another male?",
+				a:false},
+			{q:"Female donors: In the past 12 months have you had sexual contact with a male who had sexual contact with another male in the past 12 months?",
+				a:false},		
+			{q:"In the past 12 months have you had sexual contact with a person who has hepatitis?",
+				a:false},
+			{q:"In the past 12 months have you lived with a person who has hepatitis?",
+				a:false},
+			{q:"In the past 12 months have you had a tattoo?",
+				a:false},
+			{q:"In the past 12 months have you had ear or body piercing?",
+				a:false},
+			{q:"In the past 12 months have you had or been treated for syphilis or gonorrhea?",
+				a:false},
+			{q:"In the past 12 months have you been in juvenile detention, lockup, jail, or prison for more than 72 consecutive hours?",
+				a:false},
+			{q:"In the past three years have you been outside the United States or Canada?",
+				a:true},
+			{q:"From 1980 through 1996, did you spend time that adds up to three (3) months or more in the United Kingdom? REVIEW LIST OF COUNTRIES IN THE U.K.",
+				a:false},
+			{q:"From 1980 through 1996, were you a member of the U.S. military, a civilian military employee, or a dependent of a member of the U.S. military?",
+				a:false},
+			{q:"From 1980 to the present, did you spend time that adds up to five (5) years or more in Europe? (Review list of countries in Europe.)",
+				a:false},
+			{q:"From 1980 to the present, did you receive a blood transfusion in the United Kingdom or France? (Review countries list.)",
+				a:false},
+			{q:"Have you ever had a positive test for the HIV/AIDS virus?",
+				a:false},
+			{q:"Have you ever used needles to take drugs, steroids, or anything NOT prescribed by your doctor?",
+				a:false},
+			{q:"Have you ever received money, drugs, or other payment for sex?",
+				a:false},
+			{q:"Female donors: Have you ever been pregnant or are you pregnant now?",
+				a:false},		
+			{q:"Have you ever had malaria?",
+				a:false},
+			{q:"Have you ever had Chagas' disease?",
+				a:false},
+			{q:"Have you ever had Babesiosis?",
+				a:false},
+			{q:"Have you ever received a dura mater (or brain covering) graft or xenotransplantation product?",
+				a:false},
+			{q:"Have you ever had any type of cancer, including leukemia?",
+				a:false},
+			{q:"Have you ever had any problems with your heart or lungs?",
+				a:false},
+			{q:"Have you ever had a bleeding condition or a blood disease?",
+				a:false},
+			{q:"Have any of your relatives had Creutzfeldt-Jakob disease?",
+				a:false},
+			{q:"In the past 4 months, have you been exposed to a local Hepatitis A Outbreak?",
+				a:false}
+		];
+		
+		var getExistingSetting = function(question){
+			for(var int i = 0; i< config.responses.length; i++){
+				var qa = config.responses[i];
+				if(qa.q == question)
+				{
+					return qa;
+				}
+			}
+			return null;
+		}
+		
+		responses.forEach(function(item){
+			var qa = getExistingSetting(item.q);
+			if(qa){
+				item.a = qa.a;
+			}
+		});
+		config.responses = responses;
+		
 		callback(config);
 	});
 }
